484708627ca075365d8558662ec9d9e7a4c04f44
Marcelo Dias <marcelo.dias@cilia.com.br>
2022-01-29 23:51:47 -0300

    Add default value for description in migration

--- db/migrate/20220130021222_create_service_categories.rb
+++ db/migrate/20220130021222_create_service_categories.rb
@@ -2,7 +2,7 @@ class CreateServiceCategories < ActiveRecord::Migration[6.1]
   def change
     create_table :service_categories do |t|
       t.string :name
-      t.text :description
+      t.text :description, default: ''
       t.datetime :deleted_at
 
       t.timestamps
