cfc10aaddbde1e3d4c4d8da3e80073259d123c7c
Marcelo Dias <marcelo.dias@cilia.com.br>
2022-01-30 02:27:16 -0300

    Refactor clients factory

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     birthdate { Faker::Date.birthday(max_age: 130) }
-    main_phone_number { Faker::Number.decimal_part(digits: 10).to_s }
-    second_phone_number { Faker::Number.decimal_part(digits: 10).to_s }
+    main_phone_number { Faker::Number.decimal_part(digits: Client::PHONE_NUMBER_LENGTH).to_s }
+    second_phone_number { Faker::Number.decimal_part(digits: Client::PHONE_NUMBER_LENGTH).to_s }
     gender { Client::GENDERS.sample }
   end
 end
