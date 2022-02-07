293121be4b921f5e8245c2754f53e521dac132f0
Marcelo Dias <marcelo.dias@cilia.com.br>
2022-02-06 22:54:38 -0300

    Remove not yet used User attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :timeoutable, :trackable
+         :recoverable, :rememberable, :validatable
 
   ROLES = %w[admin manager employee]
   EMAIL_REGEXP = URI::MailTo::EMAIL_REGEXP
