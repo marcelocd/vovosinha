ae2c78f6145aa05bdfd444cf2992f5c9ef5b3b1d
Marcelo Dias <marcelo.dias@cilia.com.br>
2022-02-23 18:23:34 -0300

    Add to do profile_image attribute comment to SerializableUser

--- app/serializers/serializable_user.rb
+++ app/serializers/serializable_user.rb
@@ -6,12 +6,15 @@ class SerializableUser < JSONAPI::Serializable::Resource
              :last_name,
              :email,
              :birthdate,
-             :role,
-             :profile_image
+             :role
 
   attribute :full_name do
     @object.full_name
   end
+
+  # TO DO
+  # attribute :profile_image do
+  # end
 
   belongs_to :last_updated_by, class_name: 'User', optional: true
   belongs_to :deleted_by, class_name: 'User', optional: true
