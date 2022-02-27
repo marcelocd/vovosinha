0536b982336c3b5da1bac2f86d31479f7a67f095
Marcelo Dias <marcelo.dias@cilia.com.br>
2022-02-27 19:50:07 -0300

    Refactor

--- spec/factories/services.rb
+++ spec/factories/services.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     account { service_category.account }
     name { Faker::Lorem.words(number: 2).join(' ') }
     description { Faker::Lorem.paragraph(sentence_count: 2, random_sentences_to_add: 4).first(Service::MAX_DESCRIPTION_LENGTH) }
-    price_cents { Faker::Number.between(from: 1000, to: 10_000) }
+    price_cents { Faker::Number.between(from: 10_00, to: 100_00) }
     commission_percentage { Faker::Number.between(from: 0.01, to: 1.0) }
     duration_minutes { Faker::Number.between(from: 10, to: 90) }
   end
