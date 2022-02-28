c9caab456816731ce830f5c0352e3cae7c55a67b
Marcelo Dias <marcelo.dias@cilia.com.br>
2022-02-28 13:18:15 -0300

    Add active and inactive scopes to Service and ServiceOrder

--- app/models/service_order.rb
+++ app/models/service_order.rb
@@ -1,4 +1,7 @@
 class ServiceOrder < ApplicationRecord
+  scope :active, -> { where(deleted_at: nil) }
+  scope :inactive, -> { where.not(deleted_at: nil) }
+  
   belongs_to :account
   belongs_to :created_by, class_name: 'User', foreign_key: 'created_by_id'
   belongs_to :client
