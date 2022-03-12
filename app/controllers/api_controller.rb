4c590af148d1b2c0ac2e73751cae1a605d27fe05
Marcelo Dias <marcelo.dias@cilia.com.br>
2022-03-11 23:06:12 -0300

    Add http status codes to ApiController

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,9 @@
 class ApiController < ApplicationController
   include Pagy::Backend
 
+  FORBIDDEN_STATUS_CODE = 403
+  NOT_FOUND_STATUS_CODE = 404
+  UNPROCESSABLE_ENTITY_STATUS_CODE = 422
+
   skip_before_action :verify_authenticity_token, only: %i[create update destroy]
 end
